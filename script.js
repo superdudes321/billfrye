ad8b040ce4a37dcd67c01923e441596e0fa4bb86
Brad Dunbar <dunbarb2@gmail.com>
2026-04-20 19:52:40 -0400

    backgrounds

--- script.js
+++ script.js
@@ -9,7 +9,26 @@ document.addEventListener('click', (event) => {
   new Audio(element.dataset.audio).play()
 })
 
+const BACKGROUNDS = [
+  'xp',
+  'insane',
+  'frenzy',
+  'zelda',
+  'luigi',
+  'minecraft',
+  'mario64',
+]
+
 document.addEventListener('click', (event) => {
   if (!event.target.closest('footer')) return
-  document.body.classList.toggle('insane')
+
+  event.preventDefault()
+  const { body } = document
+
+  const index = BACKGROUNDS.findIndex((classname) => (
+    body.classList.contains(classname)
+  ))
+
+  body.classList.remove(...BACKGROUNDS)
+  body.classList.add(BACKGROUNDS[(index + 1) % BACKGROUNDS.length])
 })
