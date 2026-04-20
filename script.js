2cefc2c1177744e79f3473b4a42476eec2eb332c
Brad Dunbar <dunbarb2@gmail.com>
2026-04-20 19:30:31 -0400

    spaces

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 document.addEventListener('click', (event) => {
-	if (!event.target.matches('.professional,.unprofessional')) return
-	document.body.classList.toggle('flip')
+  if (!event.target.matches('.professional,.unprofessional')) return
+  document.body.classList.toggle('flip')
 })
 
 document.addEventListener('click', (event) => {
-	const element = event.target.closest('[data-audio]')
-	if (!element) return
-	new Audio(element.dataset.audio).play()
+  const element = event.target.closest('[data-audio]')
+  if (!element) return
+  new Audio(element.dataset.audio).play()
 })
-console.log('before')
+
 document.addEventListener('click', (event) => {
-	console.log('boom')
-	if (!event.target.closest('footer')) return
-	document.body.classList.toggle('insane')
+  if (!event.target.closest('footer')) return
+  document.body.classList.toggle('insane')
 })
-console.log('after')
